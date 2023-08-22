145bd93ee58a3ec943d82a7d9ab2d49d4c4786d6
Harry Pask <harry.pask118@gmail.com>
2023-08-22 15:13:01 +0100

    File import

---
+++ prepare.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+#Only to be run if a incremental backup is taken, if not then run prepare script manually
+#To run do 
+#bash prepare.bash /path/to/backup/directory 
+#script will then look for fullbackup folder and loop through the incremental backups in order
+#do not run if no incremental backups have been taken, run prepare manually
+
+# Set variables for the full backup folder and the incremental backup folder
+
+backupdir="$1"
+FULL_BACKUP_DIR=$backupdir/fullbackup/
+INCREMENTAL_BACKUP_DIR=$backupdir/incr/*
+
+
+# Change directory, unzip file and run prepare fullbackup
+cd $FULL_BACKUP_DIR
+gunzip -c $FULL_BACKUP_DIR/* | mbstream -x
+mariabackup --prepare --target-dir=$FULL_BACKUP_DIR 2>> $backupdir/perpare.log
+mv $FULL_BACKUP_DIR/full_backup.gz ..
+
+# Loop through incremental backup folders, unzip and apply them to the full backup
+
+for DIR in $INCREMENTAL_BACKUP_DIR
+do
+    cd $DIR
+    gunzip -c $DIR/* | mbstream -x
+    mariabackup --prepare --target-dir=$FULL_BACKUP_DIR --incremental-dir=$DIR 2>> $backupdir/perpare.log
+done
