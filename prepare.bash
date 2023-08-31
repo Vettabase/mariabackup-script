6a955bc848d0f12bf33abf12ed547a8880d34e2b
Harry Pask <harry.pask118@gmail.com>
2023-08-31 16:13:08 +0100

    Create prepare.bash

---
+++ prepare.bash
@@ -0,0 +1,37 @@
+#!/bin/bash
+#Only to be run if a incremental backup is taken, if not then run prepare script manually
+#To run do 
+#bash prepare.bash /path/to/backup/directory 
+#script will then look for fullbackup folder and loop through the incremental backups in order
+#do not run if no incremental backups have been taken, run prepare manually
+
+# Set variables for the full backup folder and the incremental backup folder
+
+backupdir="$1"
+FULL_BACKUP_DIR=$backupdir/fullbackup/
+INCREMENTAL_BACKUP_DIR=$backupdir/incr/*
+
+
+# Change directory, unzip file and run prepare fullbackup
+cd $FULL_BACKUP_DIR
+gunzip -c $FULL_BACKUP_DIR/* | mbstream -x
+mariabackup --prepare --target-dir=$FULL_BACKUP_DIR 2>> $backupdir/prepare.log
+mv $FULL_BACKUP_DIR/full_backup.gz ..
+
+# Loop through incremental backup folders, unzip and apply them to the full backup
+
+for DIR in $INCREMENTAL_BACKUP_DIR
+do
+    cd $DIR
+    gunzip -c $DIR/* | mbstream -x
+    mariabackup --prepare --target-dir=$FULL_BACKUP_DIR --incremental-dir=$DIR 2>> $backupdir/prepare.log
+done
+
+
+#delete incrmental uncompressed files after they are appiled to fullbackup to save space
+for DIR in $INCREMENTAL_BACKUP_DIR
+do
+    cd $DIR
+    find $DIR/* ! -name 'incremental.backup.gz' | xargs rm -rf
+    echo "Deleted uncompressed files for incremental $DIR, Run prepare script again if needed" >> $backupdir/prepare.log
+done
