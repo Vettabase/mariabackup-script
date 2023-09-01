655196de57232bd509a2f8baaf65f6cc0be1b924
Harry Pask <harry.pask118@gmail.com>
2023-09-01 14:00:21 +0100

    Update prepare.bash

--- prepare.bash
+++ prepare.bash
@@ -10,22 +10,30 @@
 backupdir="$1"
 FULL_BACKUP_DIR=$backupdir/fullbackup/
 INCREMENTAL_BACKUP_DIR=$backupdir/incr/*
-
+incrementaldir=$backupdir/incr/
+preparelog=$backupdir/prepare.log
 
 # Change directory, unzip file and run prepare fullbackup
 cd $FULL_BACKUP_DIR
 gunzip -c $FULL_BACKUP_DIR/* | mbstream -x
-mariabackup --prepare --target-dir=$FULL_BACKUP_DIR 2>> $backupdir/prepare.log
+mariabackup --prepare --target-dir=$FULL_BACKUP_DIR 2>> $preparelog
 mv $FULL_BACKUP_DIR/full_backup.gz ..
 
 # Loop through incremental backup folders, unzip and apply them to the full backup
 
 for DIR in $INCREMENTAL_BACKUP_DIR
 do
-    cd $DIR
-    gunzip -c $DIR/* | mbstream -x
-    mariabackup --prepare --target-dir=$FULL_BACKUP_DIR --incremental-dir=$DIR 2>> $backupdir/prepare.log
+    checkstatus=$(tail -n 2 $preparelog | grep -c "completed OK")
+    if [[ $checkstatus -eq 1 ]]; then
+        cd $DIR
+        gunzip -c $DIR/* | mbstream -x
+        mariabackup --prepare --target-dir=$FULL_BACKUP_DIR --incremental-dir=$DIR 2>> $preparelog
+    else
+        echo "$(date +'%Y-%m-%d %H:%M:%S') Last incremental failed to run, please check $preparelog for more details"
+        echo "$(date +'%Y-%m-%d %H:%M:%S') Check incremental folder for compressed file. Backup might be corrpted, prepare to last good incremental" >> $preparelog
+    fi
 done
+
 
 
 #delete incrmental uncompressed files after they are appiled to fullbackup to save space
@@ -33,5 +41,17 @@ for DIR in $INCREMENTAL_BACKUP_DIR
 do
     cd $DIR
     find $DIR/* ! -name 'incremental.backup.gz' | xargs rm -rf
-    echo "Deleted uncompressed files for incremental $DIR, Run prepare script again if needed" >> $backupdir/prepare.log
+    echo "$(date +'%Y-%m-%d %H:%M:%S') Deleted uncompressed files for incremental $DIR" >> $preparelog
 done
+
+lastcheckstatus=$(grep -c "completed OK" $preparelog)
+
+incbackups=$(find $incrementaldir -mindepth 1 -maxdepth 1 -type d | wc -l)
+includefull=$(($incbackups + 1))
+
+if [[ $lastcheckstatus -eq $includefull ]]; then
+    echo "$(date +'%Y-%m-%d %H:%M:%S') Prepare completed successfully and ready to restore from backup directory $FULL_BACKUP_DIR"
+else
+    echo "$(date +'%Y-%m-%d %H:%M:%S') Prepare failed, check $preparelog for more details. number of backups did not equal number of prepared backups. If you are running this a second time and didn't delete prepare.log then ignore error and check $preparelog"
+fi
+ 
